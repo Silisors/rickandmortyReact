4f4eb474c87c25b5f5c70773d9b9603ca754549b
Silisors <gamerpg08@gmail.com>
2023-11-07 16:11:22 -0500

    Mensaje del commt

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
+
 import React, { useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Characters } from "./components/characters"; // Utiliza Characters en lugar de characterData
-import { Pagination } from './components/Pagination'
-function App() {
+import { Pagination } from './components/Pagination';
+
+function useCharacterData() {
   const [characters, setCharacters] = useState([]);
   const [info, setInfo] = useState({});
-
-  const url = "https://rickandmortyapi.com/api/character";
+  const [loading, setLoading] = useState(false);
 
   const fetchCharacters = (url) => {
+    setLoading(true);
+
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCharacters(data.results);
         setInfo(data.info);
+        setLoading(false);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
   };
-
 
   const onPrevious = () => {
     fetchCharacters(info.prev);
-  }
+  };
 
   const onNext = () => {
-    fetchCharacters(info.next)
-  }
+    fetchCharacters(info.next);
+  };
 
   useEffect(() => {
-    fetchCharacters(url);
+    fetchCharacters("https://rickandmortyapi.com/api/character");
   }, []);
+
+  return { characters, info, loading, onPrevious, onNext };
+}
+
+function App() {
+  const { characters, info, loading, onPrevious, onNext } = useCharacterData();
 
   return (
     <>
       <Navbar brand="rick and morty app" />
 
       <div className="container mt-5">
-        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
-        <Characters characters={characters} /> {/* Utiliza el componente Characters aquí */}
+        {loading ? (
+          <p>Cargando...</p>
+        ) : (
+          <>
+            <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+            <Characters characters={characters} /> {/* Utiliza el componente Characters aquí */}
+            <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+          </>
+        )}
       </div>
-      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
     </>
   );
 }
