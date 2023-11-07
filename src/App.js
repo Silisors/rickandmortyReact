b30d9014280b299552375f638d5cc17299de677d
Silisors <gamerpg08@gmail.com>
2023-11-07 14:43:28 -0500

    subo todos los archivos que faltan.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Characters } from "./components/characters"; // Utiliza Characters en lugar de characterData
+import { Pagination } from './components/Pagination'
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const url = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next)
+  }
+
+  useEffect(() => {
+    fetchCharacters(url);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar brand="rick and morty app" />
+
+      <div className="container mt-5">
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+        <Characters characters={characters} /> {/* Utiliza el componente Characters aquí */}
+      </div>
+      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+    </>
   );
 }
 
